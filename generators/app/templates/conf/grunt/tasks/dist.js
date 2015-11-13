53b7ed75d5112e51b4a94e95e143113c4f794c75
Benny Neugebauer <bn@bennyn.de>
2015-11-13 12:06:25 +0100

    `grunt dist` is under development

--- generators/app/templates/conf/grunt/tasks/dist.js
+++ generators/app/templates/conf/grunt/tasks/dist.js
@@ -2,9 +2,9 @@ module.exports = function(grunt) {
   grunt.registerTask('dist', 'Create release-ready app', function() {
     grunt.task.run([
       'init',
-      'eslint:src',
-      'clean:distributables',
-      'uglify:dist'
+      // 'eslint:src',
+      // 'clean:distributables',
+      // 'uglify:dist'
     ]);
   });
 };
