3f127f860c6c8e37c53faa33e5b498ef37d564c6
Benny Neugebauer <bn@bennyn.de>
2015-11-21 22:23:44 +0100

    Removed old demo build task and changed task order for builds

--- generators/app/templates/conf/grunt/tasks/default.js
+++ generators/app/templates/conf/grunt/tasks/default.js
@@ -1,24 +1,21 @@
 module.exports = function(grunt) {
-  grunt.registerTask('default', 'Test app, transpile code and run development environment', function() {
+  grunt.registerTask('default', function() {
     var styleLanguage = grunt.config('style');
 
     grunt.task.run([
+      // Retrieve dependencies
       'init',
-      // Check & build scripts
-      'check:demo',
+      // Check scripts
       'check:main',
       'check:test',
-      // Check styles
-      'check:demo:' + styleLanguage,
-      'check:main:' + styleLanguage,
       // Build scripts
-      'build:demo',
       'build:main',
       'build:test',
+      // Check styles
+      'check:main:' + styleLanguage,
       // Build styles
-      'build:demo:' + styleLanguage,
       'build:main:' + styleLanguage,
-      // Test scripts
+      // Run tests
       'test:specs',
       // Run development environment
       'dev:open'
